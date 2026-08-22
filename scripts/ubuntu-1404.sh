1e03f3fa04f6a13339aebc12e96b4dc9702fd8de
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-08-22 16:00:12 +0300

    Update ubuntu-1404.sh

--- scripts/ubuntu-1404.sh
+++ scripts/ubuntu-1404.sh
@@ -7,4 +7,4 @@
 sudo sed -i -re 's/([a-z]{2}\.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 apt update
 apt full-upgrade -y
-grep -v '^#' ./ubuntu-1404.packages | xargs dnf install -y
+grep -v '^#' ./ubuntu-1404.packages | xargs apt install -y

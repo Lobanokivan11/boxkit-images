1400bf1c827e2ec712fd9efbb4d98197be5cdbc9
Lobanokivan11 <lobanokivan11@gmail.com>
2026-08-22 15:57:59 +0300

    TRYING TO REANIMATE UBUNTU 14.04

---
+++ scripts/ubuntu-1404.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Symlink distrobox shims
+./distrobox-shims.sh
+
+# Update the container and install packages
+sudo sed -i -re 's/([a-z]{2}\.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
+apt update
+apt full-upgrade -y
+grep -v '^#' ./ubuntu-1404.packages | xargs dnf install -y
